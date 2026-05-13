b616eebc762d3ef0e0b42c1e6021d47766870130
Yash Raj <yash.algorithmist@gmail.com>
2026-05-13 06:52:19 +0530

    Day-7

---
+++ ApnaCollege/DSA_Tutorial_with_Java/9.Arrays(Part2)/appearsTwice.java
@@ -0,0 +1,16 @@
+public class appearsTwice {
+    public static boolean twiceAppear(int arr[]){
+
+
+        return false;
+    }
+    public static void main(String [] args){
+        int arr[] = {2,5,6,7,8,9,11,23,4,7,58,44};
+        boolean tA = twiceAppear(arr);
+        if(tA){
+            System.out.println(tA);
+        }else{
+            System.out.println(tA);
+        }
+    }
+}
---
+++ ApnaCollege/DSA_Tutorial_with_Java/9.Arrays(Part2)/duplicateSet.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class duplicateSet {
+    public static boolean containsDuplicate(int [] nums){
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i< nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }else{
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+    public static void main(String [] args){
+        int arr[] = {1,2,3,4,5,3};
+        System.out.println(containsDuplicate(arr));
+    }
+}
